44a6916988c12f4012cba026b87c88a8dd07d931
Dev.NikuRamen <dev.alter0@gmail.com>
2022-12-13 11:41:50 +0900

    12-13 서브카테고리, 상품 수정

--- src/main/java/com/spring_boot/projectEx/controller/ProductController.java
+++ src/main/java/com/spring_boot/projectEx/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.spring_boot.projectEx.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
